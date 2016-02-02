f540e499032cd4d736a8d6dbb2b4bfd09c5719b2
Daniel Corregidor Coronado <Daniel.Corregidor@tajamar365.com>
2016-02-02 14:21:49 +0100

    first commit

---
+++ Geolocation/Geolocation/Inicio.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Xamarin.Forms;
+
+namespace Geolocation
+{
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var loc = CrossGeolocator.Current;
+            var pos = await loc.GetPositionAsync(10000);
+
+            if (pos == null)
+            {
+                await DisplayAlert("Error", "No hemos recibido su posicion", "Aceptar");
+                return;
+            }
+
+            else
+            {
+                TxtLat.Text = pos.Latitude.ToString();
+                TxtLong.Text = pos.Longitude.ToString();
+            }
+
+
+        }
+
+        private void ButtonDelete(object sender, EventArgs e)
+        {
+
+            TxtLat.Text = "";
+            TxtLong.Text = "";
+
+
+
+
+        }
+    }
+}
